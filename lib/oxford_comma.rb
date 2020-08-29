c3954a25a34b5570661437ecb9340f9cbd960867
Drew Thompson <drewallynthompson@gmail.com>
2020-08-29 07:42:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,7 @@ def oxford_comma(array)
 if array != nil  
  if array.length == 1 
    array.join
- elsif array.length == 2 
-   array.join(" and ")
- else array.length > 3
+ else
    array[array.length - 1] = "and " + array[array.length - 1]
   array.join(", ")
  end
