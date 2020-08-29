90b95bc9e42ea6b6365d531da1788e07b9857c9b
Drew Thompson <drewallynthompson@gmail.com>
2020-08-29 07:43:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array != nil  
  if array.length == 1 
    array.join
- else
+ else array.length > 3
    array[array.length - 1] = "and " + array[array.length - 1]
   array.join(", ")
  end
